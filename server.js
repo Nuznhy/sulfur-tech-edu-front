315689bc7222e05db25931b873ab9c12bc371a65
nuznhy <nuznhy@gmail.com>
2021-12-21 05:44:22 +0200

    finnaly

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
- 
-const path = require('path');
 const express = require('express');
+// const process = require(".eslintrc");
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(`${__dirname}/build/`));
+app.get(/.*/, (req, res) => {
+   res.sendfile(`${__dirname}/build/index.html`);
 });
-
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+app.listen(port);
+console.log('Server started...');
